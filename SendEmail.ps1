e55a7b13f060ef5077f1e818fb7f61f27437786f
romainvrs <51825966+romainvrs@users.noreply.github.com>
2022-09-15 14:52:18 +0200

    Add files via upload

---
+++ SendEmail.ps1
@@ -0,0 +1,28 @@
+﻿param (
+    [string]$Subject = "I HAZ BEEN PWN3D",
+    [string]$Body = "I HAZ BEEN PWN3D BY CYSECURIT!",
+	[string]$Recipient = ""
+)
+$userMail = ""
+try {
+	$userMail = ([adsi]"LDAP://$(whoami /fqdn)").mail
+} catch {
+	$userMail = ($env:USERNAME+"@email.com")
+	if($Recipient) { $Recipient = $targetEmailAddress }
+}
+try {
+		
+	if( $userMail ) {
+		$O = New-Object -ComObject Outlook.Application
+		$M = $O.CreateItem(0)
+		$M.Subject = $Subject
+		$M.HTMLBody = $Body
+		#$M.Recipients.Add($userMail)
+		if( $Recipient ) { 
+			$M.Recipients.Add($Recipient)
+		}
+		$M.Send()
+	}
+} catch {
+	Write-Beacon -Message $_
+}
